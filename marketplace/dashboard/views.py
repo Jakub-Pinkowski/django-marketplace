2eb7220255e27b11903088588dfc52d4c728418d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 23:17:11 +0100

    Update dashboard template path

--- marketplace/dashboard/views.py
+++ marketplace/dashboard/views.py
@@ -8,4 +8,4 @@ from item.models import Item
 def index(request):
     items = Item.objects.filter(created_by=request.user)
 
-    return render(request, "dashboard/dashboard.html", {"items": items})
+    return render(request, "dashboard/index.html", {"items": items})
