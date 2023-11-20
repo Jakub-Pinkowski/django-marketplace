915bcfc139a464e8a45b7a94a37bd1f9f1c2e9b3
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 19:32:50 +0100

    Add Category and Item models to marketplace

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -4,5 +4,21 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
