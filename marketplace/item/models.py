8670262cb81cf474cc8609bb6eb8160b2cd434e3
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-20 19:41:09 +0100

    Add Item model and register it in admin.py

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,14 +12,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Item(models.Model):
+    Category = models.ForeignKey(
+        Category, related_name="items", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="item_images", blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.name
